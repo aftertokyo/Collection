a3a259f1a066b3252d432bf070b8e109de5c6c3f
steffanie <steffanieahrensedu@gmail.com>
2022-04-20 16:25:31 +0200

    collection

---
+++ script.js
@@ -0,0 +1,42 @@
+const movies = [
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BNjViMmFiOWItODg4ZS00NmM2LTgxNGEtMjJiN2RjZWQ2ZjM3XkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_.jpg",
+        genre: "Horror/Thriller",
+        name: "American Psycho",
+        description: "A wealthy investment banker hides his psychopathic ego but later his illogical fantasies escalate and he submits to an uncontrollable bloodlust",
+        cast: [
+            "Christian Bale",
+            "Jared Leto",
+            "Willem Dafoe", ],
+        rating: "⭐ ⭐ ⭐ ⭐",
+    }
+];
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BNGJlMTVlYWQtYmM5OC00MDgwLTk3NzAtMGViY2VjOGU0YjlkXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+        genre: "Horror/Thriller",
+        name: "Titane",
+        description: "Following a series of unexplained crimes, a father is reunited with the son who has been missing for 10 years",
+        cast: [
+            "Agathe Rousselle",
+            "Vincent Lindon",
+        ],
+        rating: "⭐ ⭐ ⭐⭐ ⭐",
+    }
+];
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BZDEwYmM1MzktMzYyMy00M2VhLWJkZTUtNmIyMTcxZmRiZjQ1XkEyXkFqcGdeQXVyMTA0MjU0Ng@@._V1_FMjpg_UX1000_.jpg",
+        genre: "Psychological/Romance",
+        name: "Betty Blue",
+        description: "A lackadaisical handyman and aspiring novelist tries to support his younger girlfriend as she slowly succumbs to madness",
+        cast: [
+            "Béatrice Dalle",
+            "Vincent Lindon"],
+        rating: "⭐ ⭐ ⭐ ⭐",
+    }
+]
+
+
