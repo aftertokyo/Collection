0793f756c33256507731cb9828a57700b03b4567
steffanie <steffanieahrensedu@gmail.com>
2022-04-21 13:52:09 +0200

    collection2

---
+++ readme.md
@@ -0,0 +1,1 @@
+Exercise sheet where i had to put my previously practiced html, css, javascript & dom skills to use. The goal was to make a little "library" of things about a topic of our choice by using javascript essentially. My topic of choice was films. I have some trouble still with grasping javascript but i'll get there eventually. 
--- script.js
+++ script.js
@@ -37,6 +37,113 @@ const movies = [
             "Vincent Lindon"],
         rating: "⭐ ⭐ ⭐ ⭐",
     }
-]
+];
 
+[
+    {
+        picture: "https://upload.wikimedia.org/wikipedia/en/0/0f/Mulholland.png",
+        genre: "Drama/Thriller",
+        name: "Mulholland Drive",
+        description: "After a car wreck on the winding Mulholland Drive renders a woman amnesiac, she and a perky Hollywood-hopeful search for clues and answers across Los Angeles in a twisting venture beyond dreams and reality",
+        cast: [
+            "Naomi Watts",
+            "Laura Harring",
+            "Justin Theroux",
+        ],
+        rating: "⭐⭐⭐⭐⭐ ",
+    }
+];
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BOGIzYjM3YzMtMjk5ZS00NDY2LTllMjEtNjYwZjhmMDNhMDBkXkEyXkFqcGdeQXVyODUzMjQxMTA@._V1_FMjpg_UX1000_.jpg",
+        genre: "Comedy/Horror",
+        name: "The Rocky Horror Picture Show",
+        description: "A newly-engaged couple have a breakdown in an isolated area and must seek shelter at the bizarre residence of Dr. Frank-n-Furter",
+        cast: [
+            "Tim curry",
+            "Susan Sarandon",
+            "Barry Bostwick",
+        ],
+        rating: "⭐⭐⭐⭐⭐",
+    }
+];
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BZDBkYjYxYjUtMTdmMS00MDJhLWEyNzktYzg2OWY5NWY0Y2QyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDkzNTM2ODg@._V1_.jpg",
+        genre: "Drama/Music",
+        name: "The piano teacher",
+        description: "A young man romantically pursues his masochistic piano teacher",
+        cast: [
+            "Isabelle Huppert",
+            "Annie Girardot",
+            "Benoît Magimel",
+        ],
+        rating: "⭐⭐⭐⭐",
+    }
+];
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BMjE2OTM5OTUyOF5BMl5BanBnXkFtZTcwODI4Nzg3Nw@@._V1_.jpg",
+        genre: "Comedy/Fantasy",
+        name: "Ruby Sparks",
+        description: "A novelist struggling with writer's block finds romance in a most unusual way: by creating a female character he thinks will love him, then willing her into existence.",
+        cast: [
+            "Paul Dano",
+            "Zoe Kazan",
+            "Annette Bening",
+
+        ],
+        rating: "⭐⭐⭐⭐⭐",
+    }
+];
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BN2U1YzdhYWMtZWUzMi00OWI1LWFkM2ItNWVjM2YxMGQ2MmNhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg",
+        genre: "Thriller/Crime",
+        name: "Nightcrawler",
+        description: "When Louis Bloom, a con man desperate for work, muscles into the world of L.A. crime journalism, he blurs the line between observer and participant to become the star of his own story",
+        cast: [
+            "Jake Gyllenhaal",
+            "Rene Russo",
+            "Bill Paxton",
+         ],
+        rating: "⭐⭐⭐⭐⭐",
+    }
+];
+
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+        genre: "Drama",
+        name: "Requiem for a dream",
+        description: "The drug-induced utopias of four Coney Island people are shattered when their addictions run deep.",
+        cast: [
+            "Jared Leto",
+            "Ellen Burstyn",
+            "Jennifer Connelly",
+          ],
+        rating: "⭐⭐⭐⭐⭐",
+    }
+];
+
+
+[
+    {
+        picture: "https://m.media-amazon.com/images/M/MV5BNjgwNjkwOWYtYmM3My00NzI1LTk5OGItYWY0OTMyZTY4OTg2XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
+        genre: "Drama/Romance",
+        name: "Portrait of a lady on fire",
+        description: "On an isolated island in Brittany at the end of the eighteenth century, a female painter is obliged to paint a wedding portrait of a young woman.",
+        cast: [
+            "Noémie Merlant",
+            "Adèle Haenel",
+            "Luana Bajrami",
+          ],
+        rating: "⭐⭐⭐⭐",
+    }
+];
 
